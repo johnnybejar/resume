d44f4c17c4ccbf87984032643ec579f1ba1fcb19
johnny <jbejar2001@yahoo.com>
2023-06-22 09:47:13 -0400

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <>
+      <h2>johnny b</h2>
+      <ul>
+        <li>About Me</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </>
+  );
+}
+
+export default Header;
