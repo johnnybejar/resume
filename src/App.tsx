994097e610cf7581d83da7b13f85a9cc48a6d1ec
johnny <jbejar2001@yahoo.com>
2023-06-25 21:04:57 -0400

    some styling + contact info updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
 import Header from "./components/Header";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import "../src/styles/App.css";
 
 function App() {
   return (
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,9 +1,19 @@
+import "../styles/Contact.css";
+
 function Contact() {
   return (
-    <>
+    <div className="contact">
       <h4>contact</h4>
-      <p></p>
-    </>
+      <span>you can contact me by phone or email:</span>
+      <div className="email">
+        <img className="contact-icon" src="src/assets/email.png"></img>
+        <span>jbejar2001@yahoo.com</span>
+      </div>
+      <div className="phone">
+        <img className="contact-icon" src="src/assets/phone.png"></img>
+        <span>9106226003 (pref. text)</span>
+      </div>
+    </div>
   );
 }
 
