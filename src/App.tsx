d0766a842661f064db988e1e68e39f299b59baa1
johnnyb <jbejar2001@yahoo.com>
2023-07-07 22:16:25 -0400

    added csstransitions for smooth transitions between routes, not fully implemented yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Header from "./components/Header";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import "../src/styles/App.css";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 function App() {
+  const location = useLocation();
   return (
-    <BrowserRouter>
+    <>
       <Header />
       <div className="main">
-        <Routes>
-          <Route path="/" Component={About} />
-          <Route path="/Projects" Component={Projects} />
-          <Route path="/Contact" Component={Contact} />
-        </Routes>
+        <TransitionGroup>
+          <CSSTransition
+            key={location.pathname}
+            classNames="fade"
+            timeout={{ enter: 400, exit: 400 }}
+          >
+            <Routes>
+              <Route path="/" Component={About} />
+              <Route path="/Projects" Component={Projects} />
+              <Route path="/Contact" Component={Contact} />
+            </Routes>
+          </CSSTransition>
+        </TransitionGroup>
       </div>
       <Footer />
-    </BrowserRouter>
+    </>
   );
 }
 
