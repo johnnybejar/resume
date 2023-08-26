b5808cab1656d1aa3bbc3fb7fba8bc22e9acab11
johnnyb <jbejar2001@yahoo.com>
2023-08-26 18:59:41 -0400

    reworked the about page dev-icons to show name on hover

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -25,78 +25,114 @@ function About() {
           list of tech that i have worked with:
         </span>
         <div className="dev-icons">
-          <img
-            className="dev-icon"
-            src="static/images/devicons/typescript.svg"
-            alt="dev icon"
-            title="Typescript"
-          />
-          <img
-            className="dev-icon"
-            src="static/images/devicons/python.svg"
-            alt="dev icon"
-            title="Python"
-          />
-          <img
-            className="dev-icon"
-            src="static/images/devicons/java.svg"
-            alt="dev icon"
-            title="Java"
-          />
-          <img
-            className="dev-icon"
-            src="static/images/devicons/react.svg"
-            alt="dev icon"
-            title="React"
-          />
-          <img
-            className="dev-icon"
-            src="static/images/devicons/html.svg"
-            alt="dev icon"
-            title="HTML"
-          />
-          <img
-            className="dev-icon"
-            src="static/images/devicons/css.svg"
-            alt="dev icon"
-            title="CSS"
-          />
-          <img
-            className="dev-icon"
-            src="static/images/devicons/express.svg"
-            alt="dev icon"
-            title="Express"
-          />
-          <img
-            className="dev-icon"
-            src="static/images/devicons/php.svg"
-            alt="dev icon"
-            title="PHP"
-          />
-          <img
-            className="dev-icon"
-            src="static/images/devicons/dart.svg"
-            alt="dev icon"
-            title="Dart"
-          />
-          <img
-            className="dev-icon"
-            src="static/images/devicons/mysql.svg"
-            alt="dev icon"
-            title="MySQL"
-          />
-          <img
-            className="dev-icon"
-            src="static/images/devicons/mongodb.svg"
-            alt="dev icon"
-            title="MongoDB"
-          />
-          <img
-            className="dev-icon"
-            src="static/images/devicons/rust.svg"
-            alt="dev icon"
-            title="Rust"
-          />
+          <figure className="dev-figure">
+            <img
+              className="dev-icon"
+              src="static/images/devicons/typescript.svg"
+              alt="dev icon"
+              title="Typescript"
+            />
+            <figcaption>Typescript</figcaption>
+          </figure>
+          <figure className="dev-figure">
+            <img
+              className="dev-icon"
+              src="static/images/devicons/python.svg"
+              alt="dev icon"
+              title="Python"
+            />
+            <figcaption className="fig-caption">Python</figcaption>
+          </figure>
+          <figure className="dev-figure">
+            <img
+              className="dev-icon"
+              src="static/images/devicons/java.svg"
+              alt="dev icon"
+              title="Java"
+            />
+            <figcaption className="fig-caption">Java</figcaption>
+          </figure>
+          <figure className="dev-figure">
+            <img
+              className="dev-icon"
+              src="static/images/devicons/react.svg"
+              alt="dev icon"
+              title="React"
+            />
+            <figcaption className="fig-caption">React</figcaption>
+          </figure>
+          <figure className="dev-figure">
+            <img
+              className="dev-icon"
+              src="static/images/devicons/html.svg"
+              alt="dev icon"
+              title="HTML"
+            />
+            <figcaption className="fig-caption">HTML</figcaption>
+          </figure>
+          <figure className="dev-figure">
+            <img
+              className="dev-icon"
+              src="static/images/devicons/css.svg"
+              alt="dev icon"
+              title="CSS"
+            />
+            <figcaption className="fig-caption">CSS</figcaption>
+          </figure>
+          <figure className="dev-figure">
+            <img
+              className="dev-icon"
+              src="static/images/devicons/express.svg"
+              alt="dev icon"
+              title="Express"
+            />
+            <figcaption className="fig-caption">Express</figcaption>
+          </figure>
+          <figure className="dev-figure">
+            <img
+              className="dev-icon"
+              src="static/images/devicons/php.svg"
+              alt="dev icon"
+              title="PHP"
+            />
+            <figcaption className="fig-caption">PHP</figcaption>
+          </figure>
+          <figure className="dev-figure">
+            <img
+              className="dev-icon"
+              src="static/images/devicons/dart.svg"
+              alt="dev icon"
+              title="Dart"
+            />
+            <figcaption className="fig-caption">Dart</figcaption>
+          </figure>
+          <figure className="dev-figure">
+            <img
+              className="dev-icon"
+              src="static/images/devicons/mysql.svg"
+              alt="dev icon"
+              title="MySQL"
+            />
+            <figcaption className="fig-caption">MySQL</figcaption>
+          </figure>
+          <figure className="dev-figure">
+            <img
+              className="dev-icon"
+              src="static/images/devicons/mongodb.svg"
+              alt="dev icon"
+              title="MongoDB"
+            />
+            <figcaption className="fig-caption">MongoDB</figcaption>
+          </figure>
+          <figure className="dev-figure">
+            <img
+              className="dev-icon"
+              src="static/images/devicons/rust.svg"
+              alt="dev icon"
+              title="Rust"
+            />
+            <figcaption className="fig-caption">Rust</figcaption>
+          </figure>
         </div>
       </div>
     </motion.div>
