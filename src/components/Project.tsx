a8bd098e5716fad211e4fe0f80005a04e4fd20bf
johnnyb <jbejar2001@yahoo.com>
2024-05-21 17:02:29 -0400

    reformated projects page

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -10,21 +10,20 @@ function Project(project: Project) {
     <>
       <a className="project-link" href={project.link} target="_blank">
         <h3 className="project-header">{project.name}</h3>
+        <p className="project-description">{project.description}</p>
+        <div className="project-tech">
+          {project.icons.map((icon) => {
+            return (
+              <img
+                className="project-tech-logo"
+                src={`static/images/devicons/${icon}.svg`}
+                alt={icon}
+                title={icon.charAt(0).toUpperCase() + icon.slice(1)}
+              />
+            );
+          })}
+        </div>
       </a>
-      <p className="project-description">{project.description}</p>
-      <div className="project-tech">
-        {project.icons.map((icon) => {
-          return (
-            <img
-              className="project-tech-logo"
-              src={`static/images/devicons/${icon}.svg`}
-              alt={icon}
-              title={icon.charAt(0).toUpperCase() + icon.slice(1)}
-            />
-          );
-        })}
-      </div>
-      <div className="horizontal-rule"></div>
     </>
   );
 }
